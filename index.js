d97d5b8714cd3dfa6994c6582f044436f6b3c476
Daniel James Lewis <dantheorange@googlemail.com>
2018-03-14 22:51:59 +0000

    Basic commands implemented

--- index.js
+++ index.js
@@ -5,11 +5,26 @@ const HelperBot = require('./src/helper-bot')
 const DevMode = require('./src/middleware/dev-mode')
 const DontReplyToSelf = require('./src/middleware/dont-reply-to-self')
 const Blotto = require('./src/middleware/blotto') // blotto.js is the best js
+const Responder = require('./src/middleware/responder')
 
-bot = new HelperBot(process.env.DISCORD_BOT_TOKEN)
+const helper = new HelperBot(process.env.DISCORD_BOT_TOKEN)
 
-bot
+helper
   .use(new DevMode(['shitty-bots']))
   .use(new DontReplyToSelf())
   .use(new Blotto(['Banthro']))
+  .use(new Responder({
+    command: '/lazy',
+    response: 'is very lazy'
+  }))
+  .use(new Responder({
+    command: '/approve',
+    response: 'ᶘ ◕ᴥ◕ᶅ,b  I approve'
+  }))
+  .use(new Responder({
+    command: '/help',
+    response: (msg) => {
+      msg.reply('maybe ' + helper.bot.users.random() + ' can help you?')
+    }
+  }))
   .start()
---
+++ src/middleware/responder.js
@@ -0,0 +1,26 @@
+class Responder {
+  constructor({ command, response }) {
+    this.response = response
+    this.message = this.message.bind(this)
+
+    const regexMatcher = (str) => str.match(command)
+    const stringMatcher = (str) => str.startsWith(command)
+    const noMatcher = () => false
+
+    this.matcher = command instanceof RegExp ? regexMatcher
+      : typeof command === 'string' ? stringMatcher
+        : noMatcher
+  }
+
+  message(msg, state, kill) {
+    if (this.matcher(msg.content)) {
+      if (typeof this.response === 'function') {
+        this.response(msg)
+      } else {
+        msg.reply(this.response)
+      }
+    }
+  }
+}
+
+module.exports = Responder
